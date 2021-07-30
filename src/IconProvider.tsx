4893cca6e24c984e59a473779cca92010257cad3
李茂峰 <limaofeng@msn.com>
2021-07-30 18:39:36 +0800

    feat: 支持直接导出 store 对象

--- src/IconProvider.tsx
+++ src/IconProvider.tsx
@@ -1,24 +1,21 @@
 import { withApollo, WithApolloClient } from '@apollo/client/react/hoc';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import IconStore from './store';
 
 import './index.less';
 
-const _store = new IconStore();
-export const IconContext = React.createContext<IconStore>(_store);
+export const store = new IconStore();
+export const IconContext = React.createContext<IconStore>(store);
 
 interface IconProviderProps {
   children: React.ReactNode;
 }
 
 function IconProvider(props: WithApolloClient<IconProviderProps>) {
-  const [store] = useState(_store);
-
   const { client } = props;
 
   useEffect(() => {
     store.setClient(client!);
-
     store.loadRemote();
   }, []);
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 export { default, default as Icon } from './Icon';
-export { default as IconProvider } from './IconProvider';
+export { default as IconProvider, store } from './IconProvider';
 export { useIcon } from './hook/useIcon';
 export { useStore } from './hook/useStore';
