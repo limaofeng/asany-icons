c900366e331f4b5aab9d052b32238726670762ab
李茂峰 <limaofeng@msn.com>
2021-07-30 09:54:35 +0800

    refactor: 默认导出 Icon

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-export { default as Icon } from './Icon';
+export { default, default as Icon } from './Icon';
 export { default as IconProvider } from './IconProvider';
 export { useIcon } from './hook/useIcon';
 export { useStore } from './hook/useStore';
