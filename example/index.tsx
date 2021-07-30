6c3ae457e21f69aae7364b853c241bbf0ca0b42b
李茂峰 <limaofeng@msn.com>
2021-07-30 18:42:27 +0800

    docs: 支持直接调用 store.addIcons

--- example/index.tsx
+++ example/index.tsx
@@ -4,27 +4,22 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { Icon, IconProvider, useStore } from '../src';
+import { Icon, IconProvider, store, useStore } from '../src';
 
 import IconDisplay from './IconDisplay';
-import { useEffect } from 'react';
 
 const client = new ApolloClient({
   uri: 'http://api.asany.cn/graphql',
   cache: new InMemoryCache(),
 });
 
-const App = () => {
-  const store = useStore();
+store.addIcons([{ name: 'test', svg: '123' }]);
 
+const App = () => {
   const handleFile = e => {
     const files = e.target.files;
     store.import('302', files[0]);
   };
-
-  useEffect(() => {
-    store.addIcons([{ name: 'test', svg: '123' }]);
-  }, []);
 
   return (
     <div>
